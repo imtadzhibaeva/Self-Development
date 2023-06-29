e9111790bab0aaa9681c85e7cd873b40e78485d8
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-06-28 23:41:28 -0400

    HelloYou.java

--- Week1/HelloYou.java
+++ Week1/HelloYou.java
@@ -2,6 +2,7 @@ package Week1;
 
 public class HelloYou {
     public static void main (String[] args) {
-        System.out.println("Helo Madina!") ;
+        System.out.println("Hello Madina!") ;
+
     }
 }
