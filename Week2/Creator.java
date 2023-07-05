80d04ec4db5f97eabc441248aa6a73f98a5ee0c3
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-05 16:07:34 -0400

    Creator.java

---
+++ Week2/Creator.java
@@ -0,0 +1,16 @@
+package Week2;
+
+public class Creator {
+    public static  void main(String[] args){
+        String name = "James Gosling";
+        int yearCreated = 1995;
+        System.out.println(name);
+        System.out.println(yearCreated);
+
+
+        int yearCodecademyWasFounded = 2011; 
+        System.out.println(yearCodecademyWasFounded);
+
+    }
+}
+
