7d54b3e0b0d21024813c1bebe0bb572d5691893d
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-08 23:10:45 -0400

    Song.java

---
+++ Week2/Song.java
@@ -0,0 +1,8 @@
+package Week2;
+
+public class Song {
+	public static void main(String[] args) {   
+String openingLyrics = "Yesterday, all my troubles seemed so far away";
+System.out.println(openingLyrics);
+	}
+}
