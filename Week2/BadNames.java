91fd65484d29e3403d8be32c30eefb0f749d5a93
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-09 20:24:38 -0400

    BadNames.java

---
+++ Week2/BadNames.java
@@ -0,0 +1,13 @@
+package Week2;
+
+public class BadNames {
+    public static void main(String[] args) {
+        // String firstName = "Samira";
+        // String blah = "Smith";
+        // String com = "samira@google.com";
+        // int salaryExpectation = 100000;
+        int yearOfBirth = 1995;
+         
+        System.out.println(yearOfBirth); 
+    }
+}
