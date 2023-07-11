3a7dd114bd8c76d84049408059e047ca42867212
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-11 15:53:27 -0400

    Modulo.java

---
+++ Week3/Modulo.java
@@ -0,0 +1,9 @@
+package Week3;
+
+public class Modulo {
+    public static void main(String[] args) {
+    int students = 26;
+    int leftOut = students % 3;
+    System.out.println(leftOut);
+    }
+}
