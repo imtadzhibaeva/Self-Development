4263174e1c817da35639da63454252a689eff9d3
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-11 16:12:38 -0400

    BakeSale.java

---
+++ Week3/BakeSale.java
@@ -0,0 +1,11 @@
+package Week3;
+
+public class BakeSale {
+    public static void main(String[] args) {
+        int numCookies = 17;
+         numCookies -= 3;
+         numCookies /= 2;
+         System.out.println(numCookies);
+    }
+    
+}
