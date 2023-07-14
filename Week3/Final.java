f32f736e16970d54d673b3001acfbce3d0f2a4b4
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-14 12:22:42 -0400

    Final.java

---
+++ Week3/Final.java
@@ -0,0 +1,10 @@
+package Week3;
+
+public class Final {
+    public static void main(String[] args){
+        final double pi = 3.14;
+        System.out.println(pi);
+        // pi = 3.15; The final pi variable can not be reassigned
+    }
+    
+}
