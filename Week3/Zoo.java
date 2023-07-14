63ebba9feabaf125d193d136cc6bb9d7da8bb551
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-13 23:12:23 -0400

    Zoo.java

---
+++ Week3/Zoo.java
@@ -0,0 +1,12 @@
+package Week3;
+
+public class Zoo {
+    public static void main (String[] args) {
+     int animals = 12;
+     String species = "zabra";
+     String zooDesctription = "Our zoo has " + animals + " " + species + "s!";
+     System.out.println(zooDesctription);
+     
+    }
+    
+}
