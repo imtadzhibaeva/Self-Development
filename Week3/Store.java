ccb8fb01df6e4e390bf7d2d34ff797481ee40400
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-15 16:58:48 -0400

    Store.java

--- Week3/Store.java
+++ Week3/Store.java
@@ -11,16 +11,16 @@ public class Store {
     public Store(String product, int count, double price) {
         productType = product;
         inventoryCount = count;
-        inventoryPrice = price;
+        inventoryPrice = price; }
 
         // main method
         public static void main(String[] args) {
             Store lemonadeStand = new Store ("lemonade", 42, .99);
             Store cookieShop = new Store ("cookies", 12, 3.75);
 
-            System.out.println("Our first shop sells " + lemonadeStand.productType + " at" + lemonadeStand.inventoryPrice + " per unit.");
+            System.out.println("Our first shop sells " + lemonadeStand.productType + " at " + lemonadeStand.inventoryPrice + " per unit.");
 
             System.out.println("Our second shop has " + cookieShop.inventoryCount + " units remaining.");
-        }
+        
     }
 }
