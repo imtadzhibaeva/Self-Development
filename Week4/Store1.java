a41be5c6a952e2e379a5216e1a8be25021bd019b
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-19 13:36:02 -0400

    Store1.java

--- Week4/Store1.java
+++ Week4/Store1.java
@@ -3,15 +3,28 @@ package Week4;
 public class Store1 {
     // instance fields
     String productType;
+    // new instance fields
+    int inventoryCount;
+    double inventoryPrice;
+
 
     // constructor method
-    public Store1(String product) {
+    public Store1(String product, int count, double price) {
         productType = product;
+        // new parameters
+        inventoryCount = count;
+        inventoryPrice = price;
+
     }
 // main method
     public static void main(String[] args){
-Store1 lemonadeStand = new Store1 ("lemonade");
-System.out.println(lemonadeStand.productType);
+// Store1 lemonadeStand = new Store1 ("lemonade");
+// System.out.println(lemonadeStand.productType);
+
+Store1 cookieShop = new Store1 ("cookie", 12, 3.75);
+System.out.println(cookieShop.productType);
+System.out.println(cookieShop.inventoryCount);
+System.out.println(cookieShop.inventoryPrice);
     }
 ;
 
