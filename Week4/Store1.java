4b54d669cc3402557f359ce1e8e24e50ae9821c3
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-19 13:21:09 -0400

    Store1.java

--- Week4/Store1.java
+++ Week4/Store1.java
@@ -8,9 +8,10 @@ public class Store1 {
     public Store1(String product) {
         productType = product;
     }
-
+// main method
     public static void main(String[] args){
-
+Store1 lemonadeStand = new Store1 ("lemonade");
+System.out.println(lemonadeStand.productType);
     }
 ;
 
