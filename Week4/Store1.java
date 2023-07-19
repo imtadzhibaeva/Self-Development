5ff708ac51ed9e324386f7bbb8e08c43a94c2cf6
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-19 13:02:25 -0400

    Store!.java

---
+++ Week4/Store1.java
@@ -0,0 +1,17 @@
+package Week4;
+
+public class Store1 {
+    // instance fields
+    String productType;
+
+    // constructor method
+    public Store1(String product) {
+        productType = product;
+    }
+
+    public static void main(String[] args){
+
+    }
+;
+
+}
