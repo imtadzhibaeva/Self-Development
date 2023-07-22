568dae4da7b83b4ada2b09793c2649c77a5ce594
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-21 22:09:39 -0400

    Store2.java

---
+++ Week4/Store2.java
@@ -0,0 +1,16 @@
+package Week4;
+
+public class Store2 {
+    // instance field
+    String productType;
+
+    // constructor method
+    public Store2(String product){
+        productType = product;
+    }
+
+    // main method 
+public static void main(String[] args) {
+    
+}
+}
