7a08b0bb08b78aa9baf4457f1d41f450ae585bcc
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-07-25 23:33:49 -0400

    Store2.java

--- Week5/Store2.java
+++ Week5/Store2.java
@@ -28,6 +28,7 @@ public class Store2 {
     public static void main(String[] args) {
         Store2 lemonadeStand = new Store2("Lemonade", 3.75);
         double lemonadePrice = lemonadeStand.getPriceWithTax();
+        System.out.println(lemonadePrice);
     }
     
 
