245666a1b671e6c19fed2b23823562f59ead2c01
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-11 23:21:57 -0400

    // Deposit

--- Week6/SavingsAccount.java
+++ Week6/SavingsAccount.java
@@ -22,5 +22,12 @@ public class SavingsAccount {
     // check balance
     System.out.println("Hello");
     System.out.println("Your balance is " + savings.balance);
+
+    // Deposit
+    int afterDeposit = savings.balance + 600;
+    savings.balance = afterDeposit;
+    System.out.println("You just deposited " + 600);
+
+    
     }
 }
