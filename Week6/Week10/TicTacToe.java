70379875da97c59f6f7d314e5ad58a2136eb615c
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-14 23:26:32 -0400

    gameBoard

---
+++ Week6/Week10/TicTacToe.java
@@ -0,0 +1,14 @@
+package Week6.Week10;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+  
+    char [] [] gameBoard = {{' ', '|', ' ', '|', ' '},
+                            {'-', '+', '-', '+', '-'},
+                            {' ', '|', ' ', '|', ' '},
+                            {'-', '+', '-', '+', '-'},
+                            {' ', '|', ' ', '|', ' '}};
+
+          //printGameBoard(gameBoard);
+}
+}
