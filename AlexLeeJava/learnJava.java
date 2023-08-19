b159672262e335f14bc2fc0eb60cda3aa0b0968f
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-18 23:00:34 -0400

    primitive data types

---
+++ AlexLeeJava/learnJava.java
@@ -0,0 +1,9 @@
+public class learnJava {
+    public static void main(String[] args) {
+        // Primitive data types = build-in Java
+        int a = 5;
+        char b = 'i';
+        Long c = 400;
+        double d = 3.2;
+    }
+}
