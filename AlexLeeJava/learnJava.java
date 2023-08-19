39f367cdf5e15b5076ebf9765e103e9bba602540
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-18 23:03:49 -0400

    non-primitive data types

--- AlexLeeJava/learnJava.java
+++ AlexLeeJava/learnJava.java
@@ -1,9 +1,13 @@
 public class learnJava {
     public static void main(String[] args) {
         // Primitive data types = build-in Java
-        int a = 5;
-        char b = 'i';
-        Long c = 400;
-        double d = 3.2;
+        // int a = 5;
+        // char b = 'i';
+        // Long c = 400;
+        // double d = 3.2;
+
+        // Non-primitive data types
+        String name = "Susan";
+        System.out.println(name.toUpperCase());
     }
 }
