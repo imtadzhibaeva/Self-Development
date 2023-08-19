5715f54dd5e285d5030ab3f0aafff7af0fd0944d
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-18 23:08:07 -0400

    addExplanationPoints method

--- AlexLeeJava/learnJava.java
+++ AlexLeeJava/learnJava.java
@@ -7,7 +7,15 @@ public class learnJava {
         // double d = 3.2;
 
         // Non-primitive data types
-        String name = "Susan";
-        System.out.println(name.toUpperCase());
+        // String name = "Susan";
+        // System.out.println(name.toLowerCase());
+
+        String exclaim = addExplanationPoints("hot dogs");
+        System.out.println(exclaim);
+    }
+
+    public static void addExplanationPoints(String s){
+        System.out.println(s + "!");
+
     }
 }
