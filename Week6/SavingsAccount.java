59617338cd7df78a418bb1f9b3dc3a8e2d4539e3
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-08-23 23:37:00 -0400

    sa

--- Week6/SavingsAccount.java
+++ Week6/SavingsAccount.java
@@ -6,7 +6,6 @@ public class SavingsAccount {
     public SavingsAccount(int initialBalance){
         balance = initialBalance;
     }
-
     public static void main(String[] args) {
         SavingsAccount savings = new SavingsAccount(2000);
     
