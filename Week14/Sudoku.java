05190dd82ed109c1b46131c85626b63cda97bf72
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-09-03 23:59:06 -0400

    Sudoku

--- Week14/Sudoku.java
+++ Week14/Sudoku.java
@@ -4,4 +4,12 @@ public class  Sudoku {
 
     private final GameState gameState;
     private final int[][] gridState;
+
+    public static final GRID_BOUNDARY = 9;
+
+    public Sudoku(GameState gameState, in[][] gridState) {
+        this.gameState = gameState;
+        this gridState = gridState;
+
+    }
 }
