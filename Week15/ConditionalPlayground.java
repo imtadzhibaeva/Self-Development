600714ac5798677722fb3a0488adee07b6ab7616
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-09-06 23:47:13 -0400

    if-then statement

---
+++ Week15/ConditionalPlayground.java
@@ -0,0 +1,8 @@
+package Week15;
+ // We represent this kind of decision-making in our program using conditional or control flow statements = if-then statement
+public class ConditionalPlayground {
+    
+    public static void main (String[] args){
+
+    }
+}
