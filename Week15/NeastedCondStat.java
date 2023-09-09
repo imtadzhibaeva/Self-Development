1420aaab45046f8ac69426b9f369905a6a04de84
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-09-08 23:07:48 -0400

    Nested Conditional Statement

---
+++ Week15/NeastedCondStat.java
@@ -0,0 +1,12 @@
+package Week15;
+
+public class NeastedCondStat {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+    String coupoCode;
+  
+    public NeastedCondStat(boolean filled, double cost, String shippingMethod, String coupon) {
+        
+    }
+}
