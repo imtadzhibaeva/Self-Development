93cd885914f91497be65dcd59f11233a2fec9b88
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-09-08 23:21:47 -0400

    Nested

--- Week15/NeastedCondStat.java
+++ Week15/NeastedCondStat.java
@@ -4,12 +4,21 @@ public class NeastedCondStat {
     boolean isFilled;
     double billAmount;
     String shipping;
-    String coupoCode;
+    String couponCode;
   
     public NeastedCondStat(boolean filled, double cost, String shippingMethod, String coupon) {
         if (cost > 24.00) {
             System.out.println("High value item!");
         }
-        
+        isFilled = filled;
+        billAmount = cost;
+        shippingMethod = shippingMethod;
+        couponCode = coupon;
     }
+     public void ship() {
+        if (isFilled) {
+            System.out.println("Shipping");
+           //  System.out.println("Shipping cost " +);
+        }
+     }
 }
