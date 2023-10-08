c6e251d4c4cf484ca670bd393091caa39993cdfc
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-10-07 23:08:55 -0400

    myprofile

--- Week2/MyProfile.java
+++ Week2/MyProfile.java
@@ -1,13 +1,13 @@
-package Week2;
+// package Week2;
 
-public class MyProfile {
-    public static void main(String[] args){
-        String name = "Madina";
-        int age = 27;
-        double desiredSalary = 250.000;
-        char gender = 'f';
-        boolean lookingForJob = true;
+// public class MyProfile {
+//     public static void main(String[] args){
+//         String name = "Madina";
+//         int age = 27;
+//         double desiredSalary = 250.000;
+//         char gender = 'f';
+//         boolean lookingForJob = true;
 
-        System.out.println(name);
-    }
-}
+//         System.out.println(name);
+//     }
+// }
