703023e7b234cf87d36e75f95965b8a5fda3a4d7
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2023-10-09 21:36:40 -0400

    solution

--- Week4/Solution.java
+++ Week4/Solution.java
@@ -1,33 +1,33 @@
-package Week4;
-// Sevo Interview
-class Solution {
-    public static String findLongestSubstring(String input) {
-        int n = input.length();
-        int start = 0;
-        int maxLength = 0;
-        int[] lastIndex = new int[128]; // Assuming ASCII characters
+// package Week4;
+// // Sevo Interview
+// class Solution {
+//     public static String findLongestSubstring(String input) {
+//         int n = input.length();
+//         int start = 0;
+//         int maxLength = 0;
+//         int[] lastIndex = new int[128]; // Assuming ASCII characters
 
-        Arrays.fill(lastIndex, -1);
+//         Arrays.fill(lastIndex, -1);
 
-        for (int end = 0; end < n; end++) {
-            char currentChar = input.charAt(end);
-            start = Math.max(start, lastIndex[currentChar] + 1);
-            int currentLength = end - start + 1;
+//         for (int end = 0; end < n; end++) {
+//             char currentChar = input.charAt(end);
+//             start = Math.max(start, lastIndex[currentChar] + 1);
+//             int currentLength = end - start + 1;
 
-            if (currentLength > maxLength) {
-                maxLength = currentLength;
-            }
+//             if (currentLength > maxLength) {
+//                 maxLength = currentLength;
+//             }
 
-            lastIndex[currentChar] = end;
-        }
+//             lastIndex[currentChar] = end;
+//         }
 
-        return input.substring(start, start + maxLength);
-    }
+//         return input.substring(start, start + maxLength);
+//     }
 
-    public static void main(String[] args) {
-        String stringA = "salesforce";
-        System.out.println("stringA: " + stringA);
-        String longestSubstring = findLongestSubstring(stringA);
-        System.out.println("Longest substring without repeating characters: " + longestSubstring);
-    }
-} 
+//     public static void main(String[] args) {
+//         String stringA = "salesforce";
+//         System.out.println("stringA: " + stringA);
+//         String longestSubstring = findLongestSubstring(stringA);
+//         System.out.println("Longest substring without repeating characters: " + longestSubstring);
+//     }
+// } 
