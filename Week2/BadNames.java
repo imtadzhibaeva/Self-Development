466b289073b694c7170bd2836b6d4ab17bfcfbc9
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-05-01 23:52:27 -0400

    bad name

--- Week2/BadNames.java
+++ Week2/BadNames.java
@@ -7,7 +7,6 @@ public class BadNames {
         // String com = "samira@google.com";
         // int salaryExpectation = 100000;
         int yearOfBirth = 1995; 
-
         System.out.println(yearOfBirth); 
     }
 }
