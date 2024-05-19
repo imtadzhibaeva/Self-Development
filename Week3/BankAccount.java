776969ac7aaec30ebc37dc442079372005c64c42
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-05-18 23:18:55 -0400

    bank

--- Week3/BankAccount.java
+++ Week3/BankAccount.java
@@ -6,9 +6,8 @@ public class BankAccount {
         double amountToWithdraw = 250;
         double updatedBalance = balance - amountToWithdraw;
         double amountForEachFriend = updatedBalance / 3;
-
+        
         boolean canPurchaseTicket = amountForEachFriend >= 250;
-
         System.out.println(canPurchaseTicket);
         System.out.println("I gave each friend " + amountForEachFriend + "...");
     }
