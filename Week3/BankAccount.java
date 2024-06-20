b7251c1c1eddb34aed43ae8186ca695dd227f593
imtadzhibaeva <119200699+imtadzhibaeva@users.noreply.github.com>
2024-06-19 23:51:49 -0400

    bank account

--- Week3/BankAccount.java
+++ Week3/BankAccount.java
@@ -1,6 +1,7 @@
 package Week3;
 public class BankAccount {
     public static void main(String[] args) {
+        
         double balance = 1000.75;
         double amountToWithdraw = 250;
         double updatedBalance = balance - amountToWithdraw;
